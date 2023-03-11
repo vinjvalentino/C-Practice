2778644849f04b8e4b095c7b29d942ce13638756
vinjvalentino <125798951+vinjvalentino@users.noreply.github.com>
2023-03-11 10:55:02 +0000

    Added comments and escape sequences

--- 001-hello-world/hello.c
+++ 001-hello-world/hello.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
+    // This is a comment
+
+    /* 
+    Escape sequences:
+
+    \n = New line
+    \t = Tab
+    \\ = \
+    "\ = "
+    etc.
+    */
+    printf("Hello World! \n");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    printf("\" hi \"\n");
+    printf("\\ \\ \\");
     return 0;
 }
